774554ae0ac27511811058e0222d4448ad2a4bc2
Marchand Venter <marchand@eleo.co.za>
2021-09-14 19:08:39 +0200

    Features

---
+++ PRG522Summative7555/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PRG522Summative7555.MvcApplication" Language="C#" %>
---
+++ PRG522Summative7555/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace PRG522Summative7555.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PRG522Summative7555.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(PRG522Summative7555.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
